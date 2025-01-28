97d027615e502df68438489b9a58e94832dee504
Rodrigo <rodrigosebastianbak@hotmail.com>
2025-01-28 17:04:11 +0100

    Add edit form

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Tarea
 
 class ListaPendientes(ListView):
@@ -12,4 +14,9 @@ class DetalleTarea(DetailView):
     model = Tarea
     context_object_name = 'tarea'
     template_name = 'base/tarea.html'
+    
+class CrearTarea(CreateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
 
