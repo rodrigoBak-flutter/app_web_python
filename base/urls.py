4dd57403aee794879a6d273e599ac06b9ef3df7c
Rodrigo <rodrigosebastianbak@hotmail.com>
2025-01-28 13:57:50 +0100

    Add Urls Django

---
+++ base/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ListaPendientes, DetalleTarea
+
+urlpatterns = [
+    path('', ListaPendientes.as_view(), name='Pendientes'),
+    path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea')
+]
---
+++ base/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Tarea
+
+class ListaPendientes(ListView):
+    model = Tarea
+    context_object_name = 'tareas'
+    
+    
+class DetalleTarea(DetailView):
+    model = Tarea
+    context_object_name = 'tarea'
+    template_name = 'base/tarea.html'
+
