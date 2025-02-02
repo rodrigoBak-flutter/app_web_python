c9373514f58e98f90ae2df8df7837eae89f35014
Rodrigo <rodrigosebastianbak@hotmail.com>
2025-02-02 12:25:48 +0100

    Finish TODO list

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ListaPendientes, DetalleTarea, CrearTarea,EditarTarea, EliminarTarea,Logueo
+from .views import ListaPendientes, DetalleTarea, CrearTarea,EditarTarea, EliminarTarea,Logueo, PaginaRegistro
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [  
     path('', ListaPendientes.as_view(), name='tareas'),
     path('login/', Logueo.as_view(), name='login'),
+    path('registro/', PaginaRegistro.as_view(), name='registro'),
     path('logout/', LogoutView.as_view(next_page = 'login'), name='logout' ),
     path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea'),
     path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
--- project/settings.py
+++ project/settings.py
@@ -104,13 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOGIN_URL = 'login'
 
 
 # Static files (CSS, JavaScript, Images)
